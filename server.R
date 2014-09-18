705e11666ecbe5ac1ee7182ddc27be9a053455bf
benmwhite <bw2121@gmail.com>
2014-09-18 14:08:06 -0500

    changed graphical parameters

--- server.R
+++ server.R
@@ -2,9 +2,9 @@ library(shiny)
 library(ggplot2)
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
-    x <- 1:25
+    x <- 0:30
     probs <- dpois(x, lambda = input$mu)
-    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 0.4),
+    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 1),
           ylab = "p(x)") + theme_bw()
   })
 })
