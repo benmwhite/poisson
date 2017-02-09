121ca54bebbaa9376dd65142d196fc5692a402fd
Benjamin White <bw2121@gmail.com>
2017-02-08 22:38:59 -0600

    Update server.R
    changed y axis to (0,.5)

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x <- 0:30
     probs <- dpois(x, lambda = input$mu)
-    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 1),
+    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 0.5),
           ylab = "p(x)") + theme_bw()
   })
 })
