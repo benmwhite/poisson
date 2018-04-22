fdd1edd73b8496e7999489cc9dde8ce1f556fb2e
Benjamin White <bw2121@gmail.com>
2018-04-22 13:45:42 -0500

    Update server.R
    minor ui edits

--- server.R
+++ server.R
@@ -2,9 +2,9 @@ library(shiny)
 library(ggplot2)
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
-    x <- 0:30
+    x <- 0:25
     probs <- dpois(x, lambda = input$mu)
-    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 0.5),
-          ylab = "p(x)") + theme_bw()
+    qplot(x, probs, geom = c("point", "line"), ylim = c(0, 0.4),
+          ylab = "P(X = x)") + theme_minimal()
   })
 })
